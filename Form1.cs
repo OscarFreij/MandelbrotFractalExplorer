71e8e44f5b86b1d8fad4092f432a831812702b86
OscarFreij <otg020313@gmail.com>
2022-08-23 20:57:12 +0200

    Add multicell computation

--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,10 @@ namespace MandelbrotFractalExplorer
     {
         public Cell cell;
         public List<Cell> cellList = new List<Cell>();
-        public Form1()
+        public List<Task<Bitmap>> cellTasks = new List<Task<Bitmap>>();
+
+        
+    public Form1()
         {
             InitializeComponent();
             int id = 0;
@@ -34,10 +37,6 @@ namespace MandelbrotFractalExplorer
 
             double xStep = 0;
             double yStep = 0;
-            double xStepStart = 0;
-            double xStepEnd = 0;
-            double yStepStart = 0;
-            double yStepEnd = 0;
             double xDelta = Math.Abs(xMin - xMax);
             double yDelta = Math.Abs(yMin - yMax);
 
@@ -62,24 +61,61 @@ namespace MandelbrotFractalExplorer
                     yTempMax = yTempMax + yStep;
 
                     cellList.Add(new Cell(id, x, y, Math.Round(xTempMax,8), Math.Round(xTempMin,8), Math.Round(yTempMax,8), Math.Round(yTempMin,8), resX, resY, maxIt));
+                    id++;
                 }
             }
 
+            
             foreach (Cell item in cellList)
             {
                 System.Diagnostics.Debug.WriteLine($"IDx: {item.CellData.XId} | IDy: {item.CellData.YId} | {item.CellData.XStart} {item.CellData.XEnd} : {item.CellData.YStart} {item.CellData.YEnd}");
+                cellTasks.Add(item.Task);
             }
-            cell = new Cell(0, 0, 0, 1, -2, 1.5, -1.5, 256, 256, 255);
+            
+            //cell = new Cell(0, 0, 0, 1, -2, 1.5, -1.5, 256, 256, 255);
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
+            /*
             if (cell.Bitmap != null)
             {
                 System.Diagnostics.Debug.WriteLine("cell.Bitmap not null!");
             }
-            cell.Task.Start();
-            pixelBox1.Image = await cell.Task;
+            */
+
+            await Generate();
+
+            //cell.Task.Start();
+            
+            //pixelBox1.Image = await cell.Task;
+        }
+
+        private async Task Generate()
+        {
+            foreach (Task t in cellTasks)
+            {
+                t.Start();
+            }
+
+            List<Task<Bitmap>> indexingArray = new List<Task<Bitmap>>(cellTasks);
+            var results = new Bitmap[cellTasks.Count];
+
+            while (cellTasks.Any())
+            {
+                Task<Bitmap> completedTask = await Task.WhenAny(cellTasks).ConfigureAwait(false);
+                results[indexingArray.IndexOf(completedTask)] = completedTask.Result;
+                cellTasks.Remove(completedTask);
+            }
+
+            for (int i = 0; i < results.Length; i++)
+            {
+                System.Diagnostics.Debug.WriteLine("Displaying: " + i);
+                pixelBox1.Image = results[i];
+                Task.Delay(100).Wait();
+            }
+
+            return;
         }
     }
 }
